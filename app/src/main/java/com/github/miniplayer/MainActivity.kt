c0b63cc59b891a1c8ebd1cc332062eff4478edf2
J.K <jnuprasad@gmail.com>
2022-09-09 23:05:21 +0530

    Theme change

--- app/src/main/java/com/github/miniplayer/MainActivity.kt
+++ app/src/main/java/com/github/miniplayer/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.github.miniplayer.ui.presentation.HomeScreen
-import com.github.miniplayer.ui.theme.ParallelProcessingCoroutinesTheme
+import com.github.miniplayer.ui.theme.MiniPlayerTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ParallelProcessingCoroutinesTheme {
+            MiniPlayerTheme {
                 HomeScreen()
             }
         }
